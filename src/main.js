6e6dbf43ea11a40dc7a196c9f8f2998605444ad1
AntoineCharette <antoineocharette@gmail.com>
2021-08-23 20:00:12 -0400

    adds map feature

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 import CityForm from './cityForm';
 import CityMap from './cityMap';
@@ -8,16 +9,18 @@ export default class Main extends Component {
     super(props);
     this.state = {
       location: {},
+      map: '',
     };
   }
 
   submitFormHandler = async (event) => {
     event.preventDefault();
-    const response = await axios.get(
+    const city = await axios.get(
       `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${event.target.searchQuery.value}&format=json`
     );
     this.setState({
-      location: response.data[0],
+      location: city.data[0],
+      map: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${city.data[0].lat},${city.data[0].lon}&zoom=13`,
     });
   };
 
@@ -25,12 +28,17 @@ export default class Main extends Component {
     return (
       <>
         <CityForm submitFormHandler={this.submitFormHandler} />
-        {this.state.location.display_name && (
-          <div>
-            <h2>The city: {this.state.location.display_name}</h2>
-            <p>Lattitude: {this.state.location.lat}</p>
-            <p>Longitude: {this.state.location.lon}</p>
-          </div>
+        {this.state.map && (
+          <>
+            <Card>
+              <Card.Img src={this.state.map} alt={this.state.location.display_name} style={{ width: '600px' }} />
+              <Card.Body>
+                <Card.Title>{this.state.location.display_name}</Card.Title>
+                <Card.Text>Lattitude: {this.state.location.lat}</Card.Text>
+                <Card.Text>Longitude: {this.state.location.lon}</Card.Text>
+              </Card.Body>
+            </Card>
+          </>
         )}
         <CityMap />
       </>
